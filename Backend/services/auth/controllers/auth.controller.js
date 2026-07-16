025a131887cdc61a80e6c2f24f162af14ede15fb
kWRizzz <krishnathey2j@gmail.com>
2026-07-16 21:05:20 +0530

    interview dashboard implemented fix

--- Backend/services/auth/controllers/auth.controller.js
+++ Backend/services/auth/controllers/auth.controller.js
@@ -1,11 +1,27 @@
-
+import{
+    getAuth
+} from "firebase-admin/auth"
+import{
+    app
+} from "../config/firbase.js"
+import userModel from "../model/user.model.js"
 
 export const registerUser= async (
     req,
     res
 ) => {
     try {
-        
+        const {token} =req.body
+        const  decode= await getAuth(app).verifyIdToken(token);
+        let user= await userModel.findOne({
+            fireBaseId:decode.uid
+        })
+
+        if(!user){
+             user= await userModel.create({
+                fireBaseId:decode.uid
+             })
+        }
     } catch (error) {
         console.log(`error in registering`);
         return res.status(500).json({
--- Backend/services/auth/index.js
+++ Backend/services/auth/index.js
@@ -7,6 +7,9 @@ dotenv.config()
 connectDB()
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.get("/",(req,res)=>{
     res.send("dummy api")
 })
